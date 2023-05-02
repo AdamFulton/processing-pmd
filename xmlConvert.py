38476a1e07ba84cb8b5216e78f83d7ba5fc9e99e
Adam Fulton <adam.fulton1@students.mq.edu.au>
2023-05-02 22:56:02 +1000

    new changes

---
+++ xmlConvert.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+
+with open('output.xml', 'r') as f:
+    data = f.read()
+
+Bs_data = BeautifulSoup(data, "xml")
+
+Bs_class = Bs_data.find_all("violation")
+Bs_name = Bs_data.find_all("file")
+
+file_names = []
+for i in Bs_class:
+    name = i["class"].split("$")
+    file_names.append(name[1] + ".pde")
+
+for i in range(len(Bs_name)):
+   Bs_name[0]["name"] = file_names[0]
+
+for i in range(0,len(Bs_data.find_all("file"))):
+    Bs_data.find_all("file")[i]["name"] = file_names[i]
+
+
+
+print(Bs_data.prettify())
+
+with open("output.xml", "w") as f:
+    f.write(Bs_data.prettify())
+    f.close()
